f6f6952acda40d72b9634d20954dc9dcb937db40
gunturur <ramakanth.gunturu@gmail.com>
2023-05-07 15:28:05 -0700

    creating the files and folders needed

---
+++ daos/note.js
@@ -0,0 +1,3 @@
+// createNote(userId, noteObj) - should create a note for the given user
+// getNote(userId, noteId) - should get note for userId and noteId (_id)
+// getUserNotes(userId) - should get all notes for userId
---
+++ models/token.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const tokenSchema = new mongoose.Schema({
+  token: { type: String, required: true },
+});
+
+
+module.exports = mongoose.model("notes", tokenSchema);
+
+//A user should be able to have multiple tokens, which allows them to log in and out on multiple browsers/devices at the same time (i.e. do not use a unique index)
